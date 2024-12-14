ea65080d2ce107a68218ae31a78171a600d66299
bizaarkq <isaac.bernab.01@gmail.com>
2024-12-14 00:01:07 -0600

    ✨ without env

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,4 @@ export default defineConfig({
     site: 'https://bizaarkq.github.io',
     base: 'conteo-navidad',
     integrations: [react()],
-    env: {
-        schema: {
-            PUBLIC_SPOTIFY_CLIENT_ID: envField.string({
-                context: "client", access: "public", optional: false
-            }),
-            SPOTIFY_CLIENT_SECRET: envField.string({
-                context: "server", access: "secret"
-            }),
-        }
-    }
 });
